fb10f71cc4d52b98b093ca270d7613e2967aecfd
Erik Schmidt <eriksch@ntdev.microsoft.com>
2019-04-15 01:43:07 -0700

    Made minor tweaks to the page

--- js/quiz.js
+++ js/quiz.js
@@ -1,28 +1,28 @@
-$(document).ready(function() {
+$(document).ready(function () {
   const LA_CROIX_NAMES = [
-    {title: "Apricot", src: "images/horoscope_images/apricot.jpg"},
-    {title: "Peach - Pear", src: "images/horoscope_images/peach-pear.jpg"},
-    {title: "Ni Cola", src: "images/horoscope_images/nicola.jpg"},
-    {title: "Mure Pepino", src: "images/horoscope_images/mure-pepino.jpg"},
-    {title: "Melon Pomelo", src: "images/horoscope_images/melon-pomelo.jpg"},
-    {title: "Pure", src: "images/horoscope_images/pure.jpg"},
-    {title: "Tangerine", src: "images/horoscope_images/tangerine.jpg"},
-    {title: "Lemon", src: "images/horoscope_images/lemon.jpg"},
-    {title: "Kiwi Sandia", src: "images/horoscope_images/kiwi-sandia.jpg"},
-    {title: "Cerise Limon", src: "images/horoscope_images/cerise-limon.jpg"},
-    {title: "Berry", src: "images/horoscope_images/berry.jpg"},
-    {title: "Pamplemousse", src: "images/horoscope_images/pamplemousse.jpg"},
+    { title: "Apricot", src: "images/horoscope_images/apricot.jpg" },
+    { title: "Peach - Pear", src: "images/horoscope_images/peach-pear.jpg" },
+    { title: "Ni Cola", src: "images/horoscope_images/nicola.jpg" },
+    { title: "Mure Pepino", src: "images/horoscope_images/mure-pepino.jpg" },
+    { title: "Melon Pomelo", src: "images/horoscope_images/melon-pomelo.jpg" },
+    { title: "Pure", src: "images/horoscope_images/pure.jpg" },
+    { title: "Tangerine", src: "images/horoscope_images/tangerine.jpg" },
+    { title: "Lemon", src: "images/horoscope_images/lemon.jpg" },
+    { title: "Kiwi Sandia", src: "images/horoscope_images/kiwi-sandia.jpg" },
+    { title: "Cerise Limon", src: "images/horoscope_images/cerise-limon.jpg" },
+    { title: "Berry", src: "images/horoscope_images/berry.jpg" },
+    { title: "Pamplemousse", src: "images/horoscope_images/pamplemousse.jpg" },
   ]
 
   var $double_butts = $("#double_butts");
-  $double_butts.on("submit", function(e) {
+  $double_butts.on("submit", function (e) {
     e.preventDefault();
 
     $("#results").addClass("hidden");
     $("#result-title").text("");
 
     var sum = 0;
-    $("input:checked").each(function(x) {
+    $("input:checked").each(function (x) {
       var valuez = parseInt(this.value);
       sum += valuez;
     });
@@ -30,7 +30,7 @@ $(document).ready(function() {
     var flavor = LA_CROIX_NAMES[sum % LA_CROIX_NAMES.length];
     $("#result-title").text(flavor.title);
     $("#results img").attr("src", flavor.src);
-    $("#results").removeClass("hidden", function() {
+    $("#results").removeClass("hidden", function () {
       $("#results").focus();
     });
   });
