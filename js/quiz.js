3490c4a4c460f1d8e601a581409d6f4ae3df5447
Pat Holland <pholls@users.noreply.github.com>
2019-04-14 18:35:58 -0700

    Add beginning stuffs

---
+++ js/quiz.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  const LA_CROIX_NAMES = ["berry", "mango", "coconut", "orange", "lime"];
+
+  var $double_butts = $("#double_butts");
+  $double_butts.on("submit", function(e) {
+    e.preventDefault();
+
+    $("#results").addClass("hidden");
+    $("#result-title").text("")
+
+    var sum = 0;
+    $("input:checked").each(function(x) {
+      var valuez = parseInt(this.value);
+      sum += valuez;
+    });
+
+    var flavor = LA_CROIX_NAMES[sum % LA_CROIX_NAMES.length];
+    $("#result-title").text(flavor);
+
+    // $("#results img").attr("src", "images/" + flavor + ".png");
+    // console.log("images/" + flavor + ".png");
+  });
+});
