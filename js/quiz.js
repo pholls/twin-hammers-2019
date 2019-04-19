33edea982831bdc55ba927675f934575e9f99b36
Erik Schmidt <eriksch@ntdev.microsoft.com>
2019-04-19 00:07:42 -0700

    Updated quiz answers

--- js/quiz.js
+++ js/quiz.js
@@ -2,16 +2,16 @@ $(document).ready(function () {
   const LA_CROIX_NAMES = [
     { title: "Apricot", src: "images/horoscope_images/apricot.jpg", description: "Like Apricots, you make all the right decisions, right up until the moment when you ruin everything." },
     { title: "Peach - Pear", src: "images/horoscope_images/peach-pear.jpg", description: "You're a mystery wrapped in an enigma, like \"what's the meaning of life?\", \"what happens after we die?\" and \"shouldn't these two fruits rhyme?\"." },
-    { title: "Ni Cola", src: "images/horoscope_images/nicola.jpg", description: "This flavor is for babies. Are you a baby?" },
+    { title: "Ni Cola", src: "images/horoscope_images/nicola.jpg", description: "Confusion in a can. Like when you mean to be a soda, but end up being soda flavored soda water." },
     { title: "Mure Pepino", src: "images/horoscope_images/mure-pepino.jpg", description: "Smooth, crisp, and refreshing -- Mure Pepino, and you!" },
     { title: "Melon Pomelo", src: "images/horoscope_images/melon-pomelo.jpg", description: "You, like Melon Pomelo, are a palindrome (if you don't look too closely)." },
-    { title: "Pure", src: "images/horoscope_images/pure.jpg", description: "Bold and original, like you, Pure was initially developed as a cologne fragrance before being pivoted into a LaCroix flavor." },
-    { title: "Tangerine", src: "images/horoscope_images/tangerine.jpg", description: "\"Tangerine\" is French for \"tiny-ass orange\"." },
-    { title: "Lemon", src: "images/horoscope_images/lemon.jpg", description: "When life gives you lemons, say \"H*ck the lemons!\" and bail." },
+    { title: "Pure", src: "images/horoscope_images/pure.jpg", description: "Bold and original, like you, Pure was initially developed as a cologne fragrance before pivoting into a LaCroix flavor." },
+    { title: "Tangerine", src: "images/horoscope_images/tangerine.jpg", description: "\"Tangerine\" is French for \"baby orange\"." },
+    { title: "Lemon", src: "images/horoscope_images/lemon.jpg", description: "When life gives you lemons, say \"H*ck the lemons!\" and throw a hammer." },
     { title: "Kiwi Sandia", src: "images/horoscope_images/kiwi-sandia.jpg", description: "Kiwi is a bird. Sandia, or watermelon, is a fruit. What, do you play for Birdfruit or something?" },
     { title: "Cerise Limon", src: "images/horoscope_images/cerise-limon.jpg", description: "Cersei Limonnister, first of her name, Protector of the Seven Kingdoms." },
     { title: "Berry", src: "images/horoscope_images/berry.jpg", description: "You're quirky, like the kind of person who pronounces \"berry\" to rhyme with \"LaCroix\"." },
-    { title: "Pamplemousse", src: "images/horoscope_images/pamplemousse.jpg", description: "u r pamplemousse" },
+    { title: "Pamplemousse", src: "images/horoscope_images/pamplemousse.jpg", description: "Royalty in a can. You are the perfect complement to everything." },
   ]
 
   var $double_butts = $("#double_butts");
